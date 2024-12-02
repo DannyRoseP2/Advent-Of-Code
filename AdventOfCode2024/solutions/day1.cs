947bb92a5428e43afc2da2853b0aa99cf401fae3
DannyRoseP2 <danielr@phase2online.com>
2024-12-02 14:56:12 -0600

    Day 1 - Got the right answers but very kludgy.

--- AdventOfCode2024/solutions/day1.cs
+++ AdventOfCode2024/solutions/day1.cs
@@ -1,31 +1,55 @@
-﻿string filePath = "inputs\\day1input.txt";
-
-List<int> list1 = new List<int>();
-List<int> list2 = new List<int>();
-var total = 0;
-
-foreach (var line in File.ReadLines(filePath))
+﻿class Program
 {
-    string[] parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-    if (parts.Length == 2 &&
-        int.TryParse(parts[0], out int num1) &&
-        int.TryParse(parts[1], out int num2))
+    static void Main()
     {
-        list1.Add(num1);
-        list2.Add(num2);
+        string filePath = "inputs\\day1input.txt";
+        List<int> list1 = new List<int>();
+        List<int> list2 = new List<int>();
+
+        foreach (var line in File.ReadLines(filePath))
+        {
+            string[] parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 2 &&
+                int.TryParse(parts[0], out int num1) &&
+                int.TryParse(parts[1], out int num2))
+            {
+                list1.Add(num1);
+                list2.Add(num2);
+            }
+        }
+
+        var totalDistance = GetTotalDistance(list1, list2);
+        var similarityScore = GetSimilarityScore(list1, list2);
+        Console.WriteLine($"Part One Total :{totalDistance}");
+        Console.WriteLine($"Similarity Score :{similarityScore}");
+    }
+
+    static int GetTotalDistance(List<int> list1, List<int> list2)
+    {
+        var l1 = new List<int>(list1);
+        var l2 = new List<int>(list2);
+        var total = 0;
+        while (l1.Count > 0)
+        {
+            var min1 = l1.Min();
+            var min2 = l2.Min();
+            l1.Remove(min1);
+            l2.Remove(min2);
+            total += min1 > min2 ? (min1 - min2) : (min2 - min1);
+        }
+        return total;
+    }
+
+    static int GetSimilarityScore(List<int> list1, List<int> list2)
+    {
+        
+        var similarityScore = 0;
+        for(int i = 0; i < list1.Count; i++)
+        {
+            var num = list1[i];
+            var numCount = list2.FindAll(t => t == num).Count();
+            similarityScore += num * numCount;
+        }
+        return similarityScore;
     }
 }
-
-while (list1.Count > 0)
-{
-    var min1 = list1.Min();
-    var min2 = list2.Min();
-    list1.Remove(min1);
-    list2.Remove(min2);
-    total += min1 > min2 ? (min1 - min2) : (min2 - min1);
-}
-
-if (list1.Count == 0 && list2.Count == 0)
-    Console.WriteLine("Both lists empty");
-
-Console.WriteLine($"Total:{total}");
